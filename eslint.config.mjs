d4b1e57eac0a08529dd6575f607c497516a50cb3
maxoiduss <maxoiduss@gmail.com>
2026-02-18 02:08:05 +0000

    Properly configured eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,40 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
 
 export default [
   {
     ignores: [
-        "out",
-        "dist",
-        "**/*.d.ts"
+      "out",
+      "dist",
+      "**/*.d.ts"
     ]
   },
   {
+    files: ["src/**/*.ts"],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
         ecmaVersion: 6,
-        sourceType: "module"
+        sourceType: "module",
+        project: "./tsconfig.json",
+        tsconfigRootDir: import.meta.dirname
       }
     },
     // register plugin module; require the plugin package so ESLint can resolve it
     plugins: {
       "@typescript-eslint": tsPlugin
     },
-    
+
     rules: {
-      "@typescript-eslint/naming-convention": "warn",
-      "curly": "warn",
-      "eqeqeq": "warn",
+      "@typescript-eslint/naming-convention": [
+        "warn",
+        { "selector": "variable", "format": ["camelCase"] },
+        { "selector": "typeLike", "format": ["PascalCase"] }
+      ],
       "no-throw-literal": "warn",
-      "semi": "warn"
+      "curly": "warn",
+      "eqeqeq": "error",
+      "semi": ["error", "always"]
     }
   }
 ];
